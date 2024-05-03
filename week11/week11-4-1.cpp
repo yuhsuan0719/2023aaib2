ac13a1a3025ee313b5b6c4b90d16f45ba55f4682
yuhsuan0719 <12750272@me.mcu.edu.tw>
2024-05-03 11:51:58 +0800

    week10

---
+++ week11/week11-4-1.cpp
@@ -0,0 +1,15 @@
+// week11-4b.cpp Part 2
+#include <stdio.h>
+int main()
+{
+	int a[10][10];
+	int m, n; //part 1: Input
+	scanf("%d%d", &m, &n);
+	for(int i=0; i<m; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			scanf("%d", &a[i][j] );
+		}
+	}
+	printf("\n");
